1d05205ea34d38806f7e00526a11361834962f64
Vanessa <vanessamrios@gmail.com>
2022-09-02 15:31:28 +0200

    feat: implementa a função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Inicia o projeto"""
+    if (target_time is None or permanence_period is None):
+        return None
+
+    count = 0
+
+    for student in permanence_period:
+        if(student[0]<= target_time<= student[1]):
+            count += 1
+            
+    return count

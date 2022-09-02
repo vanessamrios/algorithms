a3161816a165e77ccb611d76de133eff33ca7c7f
Vanessa <vanessamrios@gmail.com>
2022-09-02 15:35:46 +0200

    style: adiciona whitespaces

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,7 +5,7 @@ def study_schedule(permanence_period, target_time):
     count = 0
 
     for student in permanence_period:
-        if(student[0]<= target_time<= student[1]):
+        if (student[0] <= target_time <= student[1]):
             count += 1
-            
+
     return count

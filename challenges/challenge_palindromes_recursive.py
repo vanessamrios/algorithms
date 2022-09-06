5498d727b434debebdf7ba84e395797b0eb05e26
Vanessa <vanessamrios@gmail.com>
2022-09-06 11:32:12 +0200

    style: adiciona white spaces e remove comments

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -9,10 +9,8 @@ def is_palindrome_recursive(word, low_index, high_index):
         return True
 
     if (word[low_index] == word[high_index]):
-        return is_palindrome_recursive(word, low_index +1, high_index -1)
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
     else:
         return False
 
-#GG, 0, 1
-#GG, 1, 0

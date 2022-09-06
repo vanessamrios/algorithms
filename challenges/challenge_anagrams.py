413a7e952bed68b92411308b7b123bf74154b350
Vanessa <vanessamrios@gmail.com>
2022-09-06 21:59:28 +0200

    style: corrige indentação, whitespaces e linhas longas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -4,11 +4,15 @@ def convert_string(string):
         chars.append(ord(char))
     return chars
 
+# https://en.wikipedia.org/wiki/List_of_Unicode_characters
+
+
 def quick_sort(numbers, start, end):
     if start < end:
-        p = partition(numbers, start, end) 
+        p = partition(numbers, start, end)
         quick_sort(numbers, start, p - 1)
         quick_sort(numbers, p + 1, end)
+
 
 def partition(numbers, start, end):
     pivot = numbers[end]
@@ -16,12 +20,14 @@ def partition(numbers, start, end):
 
     for index in range(start, end):
         if numbers[index] <= pivot:
-          delimiter = delimiter + 1
-          numbers[index], numbers[delimiter] = numbers[delimiter], numbers[index]
+            delimiter = delimiter + 1
+            numbers[index],
+            numbers[delimiter] = numbers[delimiter], numbers[index]
 
     numbers[delimiter + 1], numbers[end] = numbers[end], numbers[delimiter + 1]
 
     return delimiter + 1
+
 
 def is_anagram(first_string, second_string):
     if (first_string == "" or second_string == ""):
@@ -31,7 +37,7 @@ def is_anagram(first_string, second_string):
     quick_sort(first_string_converted, 0, len(first_string_converted)-1)
     quick_sort(second_string_converted, 0, len(second_string_converted)-1)
 
-    if ( first_string_converted == second_string_converted):
+    if (first_string_converted == second_string_converted):
         return True
     else:
         return False
